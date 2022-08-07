13834fd414fab7483ed6d4b214a5e40fa590b234
MarinaStepanchuk <mari20002@mail.ru>
2022-08-08 01:17:14 +0300

    add: add language change, block settings, bg from API

--- momentum/js/playList.js
+++ momentum/js/playList.js
@@ -20,3 +20,27 @@ export const playList = [
         duration: '01:00'
       }
   ]
+
+export const library = {
+  greeting: {en: 'Good ', ru: 'Добр'},
+  greetingPlace: {en: '[Enter name] ', ru: '[Введите свое имя]'},
+  morning: {en: 'morning', ru: 'ое утро'},
+  afternoon: {en: 'afternoon', ru: 'ый день'},
+  evening: {en: 'evening', ru: 'ый вечер'},
+  night: {en: 'night', ru: 'ой ночи'},
+  cityPlace: {en: '[Enter sity]', ru: '[Введите город]'},
+  wind: {en: 'Wind speed', ru: 'Скорость ветра'},
+  windUnits: {en: 'm/s', ru: 'м/с'},
+  humidity:  {en: 'Humidity', ru: 'Влажность'},
+  errorWeather: {en: 'Error! City not found for ', ru: 'Ошибка! Не найден город '},
+  nameSettingLanguage: {en: 'language', ru: 'выбор языка'},
+  sourceImageText: {en: 'Image source:', ru: 'Источник изображения'},
+  hideBlockText: {en: 'Hide blocks', ru: 'Скрыть блоки'},
+  hideTime: {en: 'Time', ru: 'Время'},
+  hideDate: {en: 'Date', ru: 'Дата'},
+  hideGreeting: {en: 'Greeting', ru: 'Приветствие'},
+  hideQuote: {en: 'Quote', ru: 'Цитата'},
+  hideWheather: {en: 'Hide blocks', ru: 'Погода'},
+  hidePlayer: {en: 'Hide blocks', ru: 'Аудиоплеер'},
+  hideTodolist: {en: 'Hide blocks', ru: 'Список дел'},
+}
--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,33 +1,106 @@
-// -------------------date, greeting------------------------------
+import {library} from './playList.js';
+import {playList} from './playList.js';
 
 const time = document.querySelector('.time');
 const dateLook = document.querySelector('.date');
 const optionsDate = {weekday: 'long', month: 'long', day: 'numeric'};
 const nameGreting = document.querySelector('.name');
 const greeting = document.querySelector('.greeting');
+const languageEn = document.querySelector('.language-en');
+const languageRu = document.querySelector('.language-ru');
+const languageSwitch = document.querySelector('.language-switch');
+const nameSettingLanguage = document.querySelector('.name-setting-language');
+const sourceImageText = document.querySelector('.source-image-text');
+const hideBlockText = document.querySelector('.hide-block-text');
+const hideTime = document.querySelector('.hide-time');
+const hideDate = document.querySelector('.hide-date');
+const hideGreeting = document.querySelector('.hide-greeting');
+const hideQuote = document.querySelector('.hide-quote');
+const hideWheather = document.querySelector('.hide-wheather');
+const hidePlayer = document.querySelector('.hide-player');
+const hideTodolist = document.querySelector('.hide-todolist');
+
+// ---------- change language --------------------
+
+let languageSelected = 'en';
+
+languageEn.addEventListener('click', () => {
+    languageSelected = 'en';
+    languageSwitch.value = '0'
+    getWeather();
+    getQuotes();
+    addText();
+});
+
+languageRu.addEventListener('click', () => {
+    languageSelected = 'ru';
+    languageSwitch.value = '1'
+    getWeather();
+    getQuotes();
+    addText();
+});
+
+languageSwitch.addEventListener('click', () => {
+    languageSwitch.value === '1' ? languageSelected = 'ru' : languageSelected = 'en';
+    getWeather();
+    getQuotes();
+    addText();
+});
+
+function addText() {
+    nameSettingLanguage.textContent = library.nameSettingLanguage[languageSelected];
+    sourceImageText.textContent = library.sourceImageText[languageSelected];
+    hideBlockText.textContent = library.hideBlockText[languageSelected];
+    hideTime.textContent = library.hideTime[languageSelected];
+    hideDate.textContent = library.hideDate[languageSelected];
+    hideGreeting.textContent = library.hideGreeting[languageSelected];
+    hideQuote.textContent = library.hideQuote[languageSelected];
+    hideWheather.textContent = library.hideWheather[languageSelected];
+    hidePlayer.textContent = library.hidePlayer[languageSelected];
+    hideTodolist.textContent = library.hideTodolist[languageSelected];
+}
+
+addText()
+
+// -------------------date, greeting------------------------------
 
 function showDate () {
     const date = new Date();
-    dateLook.textContent = date.toLocaleDateString('en-US', optionsDate);
+    dateLook.textContent = languageSelected === 'ru' ? date.toLocaleDateString('ru-RU', optionsDate): date.toLocaleDateString('en-US', optionsDate);
+}
+
+function getTimeOfDayEn() {
+    const date = new Date();
+    const hours = date.getHours();
+    if (hours < 6) {
+        return 'night'
+    } else  if (hours < 12) {
+        return 'morning'
+    } else if (hours < 18) {
+        return 'afternoon'
+    } else if (hours < 24) {
+        return 'evening'
+    }
 }
 
 function getTimeOfDay() {
     const date = new Date();
     const hours = date.getHours();
-    if (hours <= 6) {
-        return 'night'
-    } else  if (hours <= 12) {
-        return 'morning'
-    } else if (hours <= 18) {
-        return 'afternoon'
-    } else if (hours <= 24) {
-        return 'evening'
+    if (hours < 6) {
+        return library.night[languageSelected]
+    } else  if (hours < 12) {
+        return library.morning[languageSelected]
+    } else if (hours < 18) {
+        return library.afternoon[languageSelected]
+    } else if (hours < 24) {
+        return library.evening[languageSelected]
     }
 }
 
 function showGreeting() {
     const timeOfDay = getTimeOfDay();
-    greeting.textContent = `Good ${timeOfDay}`
+    nameGreting.placeholder = `${library.greetingPlace[languageSelected]}`;
+    greeting.textContent = `${library.greeting[languageSelected]}${timeOfDay}`;
 }
 
 function showDateTime () {
@@ -84,18 +157,50 @@ function getSlidePrev() {
 
 slideNext.addEventListener('click', getSlideNext);
 slidePrev.addEventListener('click', getSlidePrev);
+let tegBg = getTimeOfDayEn();
+let bgWay;
 
-function setBg() {
-    const timeOfDay = getTimeOfDay();
-    const bgNum = String(randomNum).padStart(2, '0');
+async function setBg() {
+    let tegBgGit = getTimeOfDayEn();
+    let bgNum = String(randomNum).padStart(2, '0');
     const img = new Image();
-    img.src = `https://github.com/MarinaStepanchuk/stage1-tasks/blob/assets/images/${timeOfDay}/${bgNum}.jpg?raw=true` 
+    let urlBg = {
+        GitHub: `https://github.com/MarinaStepanchuk/stage1-tasks/blob/assets/images/${tegBgGit}/${bgNum}.jpg?raw=true`,
+        UnsplashAPI : getLinkToImageUnsplash(),
+        FlickrAPI : getLinkToImageFlickr()
+    }
+    //////*********** */
+    let imgWay = await urlBg.FlickrAPI;
+    //////*********** */
+    img.src = `${imgWay}` 
     img.onload = () => {      
-        document.body.style.backgroundImage = `url('https://github.com/MarinaStepanchuk/stage1-tasks/blob/assets/images/${timeOfDay}/${bgNum}.jpg?raw=true')`
+        document.body.style.backgroundImage = `url('${imgWay}')`
     }; 
 };
 
-setBg();
+setBg()
+
+// ----------------------picture Unsplash API --------------------
+
+async function getLinkToImageUnsplash() {
+    const url = `https://api.unsplash.com/photos/random?orientation=landscape&query=${tegBg}&client_id=dCF9B8H54pt-i5Y5zUfsA0Z9-uE0AYLY6ls575BknRo`;
+    const res = await fetch(url);
+    const data = await res.json();
+    const img = new Image();
+    return data.urls.regular 
+}
+
+// ----------------------picture Flickr API --------------------
+
+async function getLinkToImageFlickr() {
+    const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=c3525b99a86813c9dbacf77485c8641d&tags=${tegBg}&extras=url_l&format=json&nojsoncallback=1`;
+    const res = await fetch(url);
+    const data = await res.json();
+    const img = new Image();
+    let bgNum = getRandomNum(0, data.photos.photo.length);
+    console.log(data.photos.photo[bgNum].url_l)
+    return data.photos.photo[bgNum].url_l 
+}
 
 // ---------------weather-------------------
 
@@ -108,12 +213,13 @@ const city = document.querySelector('.city');
 const weatherError = document.querySelector('.weather-error')
 
 async function getWeather() {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=ru&appid=6482b58158f95b17d9dce830a81efd17&units=metric`;
+    const url = languageSelected === 'en' ? `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=6482b58158f95b17d9dce830a81efd17&units=metric` : `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=ru&appid=6482b58158f95b17d9dce830a81efd17&units=metric`;
     const res = await fetch(url);
     const data = await res.json();
+    city.placeholder = `${library.cityPlace[languageSelected]}`;
     if (data.cod === '404' || data.cod === '400') {
         weatherIcon.className = 'weather-icon owf'
-        weatherError.textContent = `Error! city not found for '${city.value}'!`;
+        weatherError.textContent = `${library.errorWeather[languageSelected]}'${city.value}'!`;
         temperature.textContent = '';
         weatherDescription.textContent = '';
         wind.textContent = '';
@@ -122,9 +228,9 @@ async function getWeather() {
         weatherIcon.className = 'weather-icon owf'
         weatherIcon.classList.add(`owf-${data.weather[0].id}`);
         temperature.textContent = `${Math.floor(data.main.temp)}°C`;
-        weatherDescription.textContent = data.weather[0].main;
-        wind.textContent = `Wind speed: ${Math.floor(data.wind.speed)} m/s`;
-        humidity.textContent = `Humidity: ${data.main.humidity}%`;
+        weatherDescription.textContent = data.weather[0].description;
+        wind.textContent = `${library.wind[languageSelected]}: ${Math.floor(data.wind.speed)} ${library.windUnits[languageSelected]}`;
+        humidity.textContent = `${library.humidity[languageSelected]}: ${data.main.humidity}%`;
         weatherError.textContent = '';
     }
 };
@@ -144,17 +250,20 @@ const quote = document.querySelector('.quote');
 const author = document.querySelector('.author');
 const changeQuote = document.querySelector('.change-quote');
 let rep;
+let numberQuote;
 
-async function getQuotes() {  
-    const quotes = 'js/data.json';
+async function getQuotes(change) {
+    const quotes = languageSelected === 'en' ? 'js/data.json' : 'js/dataRu.json';
     const res = await fetch(quotes);
     const data = await res.json(); 
-    let numberQuote = getRandomNum(0, data.length);
-    if (numberQuote === rep) { 
-        if (numberQuote < data.length-1) {
-            numberQuote += 1;
-        } else {
-            numberQuote -= 1;
+    if(change) {
+        numberQuote = getRandomNum(0, data.length);
+        if (numberQuote === rep) { 
+            if (numberQuote < data.length-1) {
+                numberQuote += 1;
+            } else {
+                numberQuote -= 1;
+            }
         }
     }
     quote.textContent = `"${data[numberQuote].text}"`;
@@ -162,21 +271,22 @@ async function getQuotes() {
     rep = numberQuote;
 }
 
-getQuotes();
+getQuotes(true);
 
-changeQuote.addEventListener('click', getQuotes)
+changeQuote.addEventListener('click', () => getQuotes(true))
 
 
 //---------------------audio player------------------
-
-import {playList} from './playList.js';
 
 const play = document.querySelector('.play');
 const playPrev = document.querySelector('.play-prev');
 const playNext = document.querySelector('.play-next');
 const playListContainer = document.querySelector('.play-list');
 const soundTrack = document.querySelector('.sound-track')
-const duration = document.querySelectorAll('.duration')
+const duration = document.querySelector('.duration');
+const volumeSlider = document.querySelector(".sound-volume");
+const timeline = document.querySelector('.timeline');
+const buttonVolume = document.querySelector('.button-volume')
 
 playList.forEach(element => {
     const li = document.createElement('li');
@@ -192,31 +302,42 @@ let isPlay = false;
 
 let playNum = 0;
 soundTrack.textContent = `${playList[0].title}`;
-duration.textContent = `${playList[0].duration}`
-console.log(duration.textContent)
+duration.textContent = `${playList[0].duration}`;
+audio.volume = 0.75;
+volumeSlider.value = Number(audio.volume);
+
+volumeSlider.addEventListener('click', e => {
+    volumeSlider.value === '0' ? buttonVolume.classList.add('button-volume-off') :  buttonVolume.classList.remove('button-volume-off');
+    audio.volume = volumeSlider.value
+})
 
 function playAudio() {
+    let stopPoint = audio.currentTime
     audio.src = playList[playNum].src;
-    audio.currentTime = 0;
-    isPlay ? audio.pause() : audio.play();
+    if(isPlay) {
+        audio.currentTime = stopPoint;
+        audio.pause()
+     } else {
+        audio.currentTime = stopPoint;
+        audio.play();
+     } 
     isPlay = !isPlay
     play.classList.toggle('pause');
     playItem[playNum].classList.toggle('item-active');
     playItem[playNum].classList.add('item-active-play');
     soundTrack.textContent = `${playList[playNum].title}`;
-    // duration.textContent = `${playList[playNum].duration}`;
+    duration.textContent = `${playList[playNum].duration}`;
 };
-  
+
 function playSwitch() {
     audio.src = playList[playNum].src;
-    audio.currentTime = 0;
     audio.play();
     isPlay = true;
     play.classList.add('pause');
     playItem[playNum].classList.add('item-active');
     playItem[playNum].classList.add('item-active-play');
     soundTrack.textContent = `${playList[playNum].title}`;
-    // duration.textContent = `${playList[playNum].duration}`;
+    duration.textContent = `${playList[playNum].duration}`;
 };
 
 function playNextAudio() {
@@ -237,10 +358,24 @@ function playPrevAudio() {
     playSwitch();
 }
 
+let pointVolume;
+
 play.addEventListener('click', playAudio);
 playPrev.addEventListener('click', playPrevAudio);
 playNext.addEventListener('click', playNextAudio);
 audio.addEventListener('ended', playNextAudio);
+buttonVolume.addEventListener('click', () => {
+    if(audio.volume > 0) {
+        pointVolume = audio.volume;
+        volumeSlider.value = '0';
+        audio.volume = Number(volumeSlider.value);
+        buttonVolume.classList.add('button-volume-off');
+    } else {
+        buttonVolume.classList.remove('button-volume-off');
+        volumeSlider.value = pointVolume;
+        audio.volume = volumeSlider.value;
+    }
+})
 
 playListContainer.addEventListener('click', (event,index) => {
     if (event.target.className === 'play-item') {
@@ -257,15 +392,50 @@ playListContainer.addEventListener('click', (event,index) => {
             play.classList.toggle('pause');
             playItem[playNum].classList.remove('item-active');
             soundTrack.textContent = `${playList[playNum].title}`;
-            // duration.textContent = `${playList[playNum].duration}`;
+            duration.textContent = `${playList[playNum].duration}`;
             isPlay = false;
         } else {
             audio.play();
             play.classList.toggle('pause');
             playItem[playNum].classList.add('item-active');
             soundTrack.textContent = `${playList[playNum].title}`;
-            // duration.textContent = `${playList[playNum].duration}`;
+            duration.textContent = `${playList[playNum].duration}`;
             isPlay = true;
         }
     }
-})
+});
+
+function getTimeCodeFromNum(num) {
+    let seconds = parseInt(num);
+    let minutes = parseInt(seconds / 60);
+    seconds -= minutes * 60;
+    const hours = parseInt(minutes / 60);
+    minutes -= hours * 60;
+  
+    if (hours === 0) return `${minutes}:${String(seconds % 60).padStart(2, 0)}`;
+    return `${String(hours).padStart(2, 0)}:${minutes}:${String(
+      seconds % 60
+    ).padStart(2, 0)}`;
+}
+
+function getSecond(dur) {
+    let second = Number(dur.substr(0,1))*10*60+Number(dur.substr(1,1))*60+Number(dur.substr(3,1))*10+Number(dur.substr(4,1))
+    return second;
+}
+
+timeline.addEventListener('click', e => {
+    const timelineWidth = window.getComputedStyle(timeline).width;
+    const durSound = getSecond(playList[playNum].duration)
+    const timeToSeek = e.offsetX / parseInt(timelineWidth) * durSound;
+    audio.currentTime = timeToSeek;
+});
+
+setInterval(() => {
+    const progressBar = document.querySelector('.duration-sound');
+    const durSound = getSecond(playList[playNum].duration)
+    progressBar.style.width = audio.currentTime / durSound * 100 + "%";
+    const timerPlay = document.querySelector('.timer-play');
+    timerPlay.textContent = getTimeCodeFromNum(
+      audio.currentTime
+    );
+}, 500);
